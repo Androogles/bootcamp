5517062a4b8d918305cf227784e0bd25c6539d6f
Androogles <35560498+Androogles@users.noreply.github.com>
2018-01-19 13:24:01 +0100

    betingelser

---
+++ betingelser.js
@@ -0,0 +1,58 @@
+// Betingelser
+
+// Øvelse 2
+// let bolean = false;
+
+// if (bolean == true) {
+//     console.log("hej verden");
+// }
+// else {
+//     console.log("Falsk");
+// }
+
+// // Øvelse 3
+// let tal = 6;
+// console.log(tal);
+
+// if (tal > 5) {
+//     console.log(8);
+// }
+
+// Øvelse 4
+// let workHarder = true;
+// let result = workHarder ? "Jep, der skal arbejdes hårdere" : "Nope, der arbejdes fint";
+// console.log(result);
+
+// Øvelse 5
+// let Læskedrik = "Cola";
+
+// if (Læskedrik == "FaxeKondi") {
+//     console.log("Det min yndlings sodavand");
+// }
+// else if (Læskedrik == "Cola") {
+//     console.log("Det ikke min, yndlingsodavand");
+// }
+
+// else if (Læskedrik == "Sprite") {
+//     console.log("Heller ikke den");
+// }
+
+// Øvelse 6
+// let name = "Egon";
+// switch (name) {
+//    case "Egon":
+//       console.log(name, 'han har en plan');
+//       break;
+//    case "Finn":
+//       console.log(name, 'ønsker medvind på cykelstierne');
+//       break;
+//    case "Georg":
+//       console.log(name, 'får en lys ide');
+
+//    case "Hjælper":
+//       console.log(name, 'er lille');
+//       break;
+//    default:
+//       console.log(name, 'er ukendt');
+//       break;
+// }

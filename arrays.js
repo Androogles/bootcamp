9e8947f21f90c6d9555697ba972e3dbcc3bb25af
Androogles <35560498+Androogles@users.noreply.github.com>
2018-01-19 11:18:21 +0100

    arrays

---
+++ arrays.js
@@ -0,0 +1,29 @@
+//Øvelse 1 Arrays..
+
+// // Øvelse 2
+// let frugter = ["pære", "æble", "banan"];
+// console.log(frugter);
+
+// // // Øvelse 3
+// // console.log(frugter[1]);
+
+// // Øvelse 4
+// frugter[3] = "fersken";
+// console.log(frugter);
+
+// Øvelse 5
+// console.log(frugter.length);
+
+// Øvelse 6
+// frugter[3] = "Appelsin";
+// console.log(frugter);
+
+// Øvelse 7
+let personer = [["Andreas", 24, "Alberslund"], ["Henning", 35, "Smørum"], ["Anders", 28, "Glostrup"]];
+console.log(personer);
+
+
+
+
+
+

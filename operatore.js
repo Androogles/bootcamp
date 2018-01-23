5f52595568d78bfd9caf4e2e5d99026b033df450
Androogles <35560498+Androogles@users.noreply.github.com>
2018-01-23 14:48:06 +0100

    operatore

---
+++ operatore.js
@@ -0,0 +1,82 @@
+//Operatorer
+
+//Øvelse 2
+
+let Nr1 = 5;
+let Nr2 = 8;
+
+let første = 5;
+let anden = 3;
+
+
+// Plus
+console.log(Nr1 + Nr2);
+// Minus
+console.log(Nr1 - Nr2);
+// Gange
+console.log(Nr1 * Nr2);
+// Dividere
+console.log(Nr1 / Nr2);
+// Modulus
+console.log(Nr1 % Nr2);
+// ++ Operater
+første++;
+console.log(første);
+// -- Operater
+anden--;
+console.log(anden);
+
+//Øvelse 3 Assignment Operators
+
+//Eksempel 1
+let x = 2;
+let y = 5;
+x += y;
+console.log(x);
+
+// Eksempel 2 tildeler en ny værdi til variablen.
+let x2 = 2;
+let y2 = 5;
+x2 = y2;
+console.log(x2, y2);
+
+// Eksempel 3
+let x3 = 2;
+let y3 = 5;
+x3 -= y3;
+console.log(x3, y3);
+
+// Eksempel 4
+let x4 = 2;
+let y4 = 5;
+x4 *= y4;
+console.log(x4, y4);
+
+// Eksempel 5 
+let x5 = 2;
+let y5 = 5;
+x5 %= y5;
+console.log(x5, y5);
+
+
+//Øvelse 4
+let number = 15;
+let name = "Hans";
+
+if (number > 10 && name == "Hans") {
+    console.log("højere end 10", name);
+} else {
+    console.log("minde end 10, ingen Hans");
+}
+
+//Øvelse 5
+let number = 1;
+let name = "Hans";
+
+if (number >= 10 || name == "Hans") {
+    console.log("højere end 10", name);
+} else {
+    console.log("minde end 10, ingen Hans");
+}
+
+

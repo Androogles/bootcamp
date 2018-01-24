f308cee441f9288003d40da80e5af9b04316287f
Androogles <35560498+Androogles@users.noreply.github.com>
2018-01-24 12:40:53 +0100

    løkker

--- løkker.js
+++ løkker.js
@@ -1,69 +1,72 @@
-// // // Løkker
-// // //Øvelse 2
+// Løkker
+// Øvelse 2
 
 
-// // // While
-// let text = "";
-// let i = 0;
+// While
+let text = "";
+let i = 0;
 
-// while (i < 25) {
-//     i++;
-//     console.log(" Nummer " + i);
-// }
+while (i < 25) {
+    i++;
+    console.log(" Nummer " + i);
+}
 
-// // // For
-// let text = "";
-// let i = 0;
+// For
+let text = "";
+let i = 0;
 
-// for (i = 0; i <= 25; i++) {
-//     console.log(i);
-// }
+for (i = 0; i <= 25; i++) {
+    console.log(i);
+}
 
-// // // Do While
-// let text = "nummer";
-// let i = 0;
+// Do While
+let text = "nummer";
+let i = 0;
 
-// do {
-//     console.log(text, i);
-//     i++;
-// } while (i <= 25);
+do {
+    console.log(text, i);
+    i++;
+} while (i <= 25);
 
-// // //Øvelse 3
-// let text = "nummer";
-// let i = 0;
+// Øvelse 3
+let text = "nummer";
+let i = 0;
 
-// do {
-//     i++;
-//     console.log(text, i);
-// } while (i % 25);
+do {
+    i++;
+    console.log(text, i);
+} while (i % 25);
 
-// // //Øvelse 4
-// for (let i = 2017; i >= 1917; i--) {
-//     console.log(i);
-// }
+// Øvelse 4
+for (let i = 2017; i >= 1917; i--) {
+    console.log(i);
+}
 
-// // //Øvelse 5
-// let ugedage = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
+// Øvelse 5
+let ugedage = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
 
-// ugedage.forEach(function (dage, index) {
-//     console.log("idag er det " + dage + " Nr " + index);
-// });
+ugedage.forEach(function (dage, index) {
+    console.log("idag er det " + dage + " Nr " + index);
+});
 
-// // //Øvelse 7
-// let navne = ["Andreas", "Marcus", "Marc", "Nicolai"];
+// Øvelse 6 i MarkDown
 
-// navne.forEach(function (navn) {
-//     console.log("Mit Navn er: " + navn);
-// });
+// Løkker Del 2
+// Øvelse 7
+let navne = ["Andreas", "Marcus", "Marc", "Nicolai"];
 
-// // //Øvelse 8
-// let navne = ["Volbeat", "Eminem", "Xibit", "Mozart"];
+navne.forEach(function (navn) {
+    console.log("Mit Navn er: " + navn);
+});
 
-// navne.forEach(function (navn, index) {
-//     console.log(navn + " Nr " + index);
-// });
+// //Øvelse 8
+let navne = ["Volbeat", "Eminem", "Xibit", "Mozart"];
 
-// //Øvelse 9  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+navne.forEach(function (navn, index) {
+    console.log(navn + " Nr " + index);
+});
+
+//Øvelse 9  
 let tal = [1, 2, 3, 40, 5, 6, 7, 8, 9, 10];
 
 tal.forEach(function (tjo, index, arr) {

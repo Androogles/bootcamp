9aaa2bc0ebe79cc9a5a486e6741c2dd39b384b08
Androogles <35560498+Androogles@users.noreply.github.com>
2018-01-24 12:41:35 +0100

    variabler

--- Variabler.js
+++ Variabler.js
@@ -1,51 +1,51 @@
-// Øvelse 1 ...
+// Variabler Opgaver
 
 // Øvelse 2
-// let navn = "Andreas";
-// console.log(navn);
+let navn = "Andreas";
+console.log(navn);
 
 // Øvelse 3
-// let alder = 24;
-// console.log(alder);
+let alder = 24;
+console.log(alder);
 
 // Øvelse 4 stavefejl, bynavn er ikke defineret.
-// let byNavn = "Roskilde";
-// console.log(byNavn);
+let byNavn = "Roskilde";
+console.log(byNavn);
 
 // Øvelse 5 
-// let x = 5;
-// let y = 10;
-// console.log(y + x);
+let x = 5;
+let y = 10;
+console.log(y + x);
 
 // Øvelse 6
-// let x = 5;
-// let y = 10;
-// let z = y + x;
-// console.log(z);
+let x = 5;
+let y = 10;
+let z = y + x;
+console.log(z);
 
 // Øvelse 7
-// let mitFornavn = "Andreas";
-// let mitEfternavn = "Kraft";
-// let minAlder = 24;
-// console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn + ' og jeg er ' + minAlder + ' år');
+let mitFornavn = "Andreas";
+let mitEfternavn = "Kraft";
+let minAlder = 24;
+console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn + ' og jeg er ' + minAlder + ' år');
 
 // Øvelse 8 
-// let Moms = 1.25;
-// let PrisUdenMoms = 400;
-// let PrisMedMoms = PrisUdenMoms * Moms;
-// console.log(PrisMedMoms);
+let Moms = 1.25;
+let PrisUdenMoms = 400;
+let PrisMedMoms = PrisUdenMoms * Moms;
+console.log(PrisMedMoms);
 
-// let PrisUdenMoms2 = PrisUdenMoms / Moms;
-// console.log(PrisUdenMoms2);
+let PrisUdenMoms2 = PrisUdenMoms / Moms;
+console.log(PrisUdenMoms2);
 
 // Øvelse 9
-// let første = '10';
-// let anden = '20';
+let første = '10';
+let anden = '20';
 
-// let byt = første;
-// første = anden;
-// anden = byt;
-// console.log(første, anden);
+let byt = første;
+første = anden;
+anden = byt;
+console.log(første, anden);
 
 
 
